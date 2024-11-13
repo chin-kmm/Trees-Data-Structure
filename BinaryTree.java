fc68ac4788c4e5f1b32869798b496a4d669b222b
chin-kmm <chenylcabillada1@gmail.com>
2024-11-13 21:49:49 +0800

    Added Tree Data Structure

---
+++ BinaryTree.java
@@ -0,0 +1,62 @@
+
+public class BinaryTree {
+    class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    BinaryTree() {
+        root = null;
+    }
+
+    // Function to insert a new node into the binary tree
+    void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    Node insertRec(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
+    // Function to perform inorder traversal of the binary tree
+    void inorderTraversal(Node root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            System.out.print(root.data + " ");
+            inorderTraversal(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(70);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(60);
+        tree.insert(80);
+
+        System.out.println("Inorder traversal of the binary tree:");
+        tree.inorderTraversal(tree.root);
+    }
+}
